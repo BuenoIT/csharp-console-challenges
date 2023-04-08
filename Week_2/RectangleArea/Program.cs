05e2aa8d6db6a7064b651e59b82e2785511dd6c8
BuenoIT <bueno.itdev@gmail.com>
2023-04-08 11:36:30 -0400

    Week 2 Content Added

---
+++ Week_2/RectangleArea/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to input the length and width of the rectangle
+            Console.Write("Enter the length of the rectangle: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            // Calculate the area of the rectangle
+            double area = length * width;
+
+            // Output the area of the rectangle
+            Console.WriteLine("The area of the rectangle is: " + area);
+            Console.ReadKey();
+        }
+    }
+}
