358c471680fc522f88be5002e98c578cd3291c35
BuenoIT <bueno.itdev@gmail.com>
2023-04-10 11:04:43 -0400

    Week 7 Content

---
+++ Week_7/MathQuiz/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                // Generate a random math question
+                int a = GenerateRandomNumber(1, 10);
+                int b = GenerateRandomNumber(1, 10);
+                int result = 0;
+                char op = GetRandomOperator();
+
+                switch (op)
+                {
+                    case '+':
+                        result = a + b;
+                        break;
+                    case '-':
+                        result = a - b;
+                        break;
+                    case '*':
+                        result = a * b;
+                        break;
+                    case '/':
+                        result = a / b;
+                        break;
+                }
+
+                // Ask the user for the answer
+                Console.Write($"{i}. What is {a} {op} {b}? ");
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                // Check the answer
+                if (answer == result)
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, the correct answer is {result}.");
+                }
+
+                Console.WriteLine();
+            }
+
+            // Display the final score
+            Console.WriteLine($"You scored {score} out of 10.");
+            Console.ReadKey();
+        }
+
+        static int GenerateRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1);
+        }
+
+        static char GetRandomOperator()
+        {
+            char[] operators = { '+', '-', '*', '/' };
+            Random random = new Random();
+            return operators[random.Next(operators.Length)];
+        }
+    }
+}
---
+++ Week_7/README.md
@@ -0,0 +1,23 @@
+# Week 7
+
+Review
+
+## Challenge #1
+
+Title: Basic Math Quiz
+
+Description: In this challenge, you will create a simple math quiz program that tests the user's knowledge of variables, operators, decision statements, methods, and iteration in C#. The program will generate a series of math questions that the user must answer correctly.
+
+Here are the requirements for the program:
+
+1. The program should generate 10 random math questions, using variables and operators such as +, -, *, and /.
+
+2. Each question should be displayed to the user, along with a prompt to enter their answer.
+
+3. After the user has answered all 10 questions, the program should display their score (i.e. the number of questions they answered correctly out of 10).
+
+4. The program should use decision statements (such as if/else) to determine if the user's answer is correct or not.
+
+5. The program should use methods to generate the random math questions and to check the user's answers.
+
+6. The program should use iteration (such as a for loop) to generate and display the 10 math questions.
