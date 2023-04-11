cec7f8c4f73acbd2d74e82c205ac7eb9ed381a4c
BuenoIT <bueno.itdev@gmail.com>
2023-04-11 13:00:18 -0400

    String Manipulation Challenge

---
+++ Week_13/StringAnalyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> texts = new List<string>() { "The code is code another code", "The coder is experienced", "I am a coder too", "Coding is fun" };
+            StringAnalyzerClass analyzer = new StringAnalyzerClass();
+
+            foreach (string text in texts)
+            {
+                int count = analyzer.CountOccurrences(text, "code");
+                Console.WriteLine("\"" + text + "\" contains " + count + " occurrence(s) of \"code\".");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week_13/StringAnalyzer/StringAnalyzerClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAnalyzer
+{
+    internal class StringAnalyzerClass
+    {
+        public int CountOccurrences(string text, string substring)
+        {
+            int count = 0;
+            int index = 0;
+            while (index < text.Length)
+            {
+                if (text.Substring(index).StartsWith(substring))
+                {
+                    index += substring.Length;
+                    count++;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            return count;
+        }
+    }
+}
