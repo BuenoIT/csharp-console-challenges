3040e9afc2e7cc50e2d48a913ff813920c043c2c
BuenoIT <bueno.itdev@gmail.com>
2023-04-14 10:42:32 -0400

    PasswordChecker Solution

---
+++ Week_13/PasswordStrengthChecker/PasswordStrengthChecker.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordStrengthChecker
+{
+    internal class PasswordStrengthChecker
+    {
+        private string password;
+
+        public PasswordStrengthChecker(string password)
+        {
+            this.password = password;
+        }
+
+        public void SetPassword(string password)
+        {
+            this.password = password;
+        }
+
+        public string GetPassword()
+        {
+            return password;
+        }
+
+        public bool CheckLength()
+        {
+            return password.Length >= 8;
+        }
+
+        public bool CheckUppercase()
+        {
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool CheckLowercase()
+        {
+            foreach (char c in password)
+            {
+                if (char.IsLower(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool CheckDigits()
+        {
+            foreach (char c in password)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool CheckSpecialChars()
+        {
+            string specialChars = "!@#$%^&*(),.?\"':{}|<>";
+            foreach (char c in password)
+            {
+                if (specialChars.Contains(c.ToString()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsStrongPassword()
+        {
+            return CheckLength() && CheckUppercase() && CheckLowercase() && CheckDigits() && CheckSpecialChars();
+        }
+
+        public static string GenerateRandomPassword(int length)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(),.?\"':{}|<>";
+            Random random = new Random();
+            char[] password = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(password);
+        }
+    }
+}
---
+++ Week_13/PasswordStrengthChecker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordStrengthChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Generate a random password of length 10
+            string randomPassword = PasswordStrengthChecker.GenerateRandomPassword(10);
+
+            Console.WriteLine("Generated Random Password: " + randomPassword);
+
+            // Create an instance of PasswordStrengthChecker
+            PasswordStrengthChecker passwordChecker = new PasswordStrengthChecker(randomPassword);
+
+            // Check password strength
+            Console.WriteLine("Password Length: " + passwordChecker.CheckLength());
+            Console.WriteLine("Contains Uppercase: " + passwordChecker.CheckUppercase());
+            Console.WriteLine("Contains Lowercase: " + passwordChecker.CheckLowercase());
+            Console.WriteLine("Contains Digits: " + passwordChecker.CheckDigits());
+            Console.WriteLine("Contains Special Characters: " + passwordChecker.CheckSpecialChars());
+            Console.WriteLine("Is Strong Password: " + passwordChecker.IsStrongPassword());
+
+            // Set a new password
+            passwordChecker.SetPassword("MyNewPassword123!");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Password: MyNewPassword123!");
+
+            // Check password strength again
+            Console.WriteLine("Password Length: " + passwordChecker.CheckLength());
+            Console.WriteLine("Contains Uppercase: " + passwordChecker.CheckUppercase());
+            Console.WriteLine("Contains Lowercase: " + passwordChecker.CheckLowercase());
+            Console.WriteLine("Contains Digits: " + passwordChecker.CheckDigits());
+            Console.WriteLine("Contains Special Characters: " + passwordChecker.CheckSpecialChars());
+            Console.WriteLine("Is Strong Password: " + passwordChecker.IsStrongPassword());
+
+            Console.ReadKey();
+        }
+    }
+}
